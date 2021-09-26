f648edad6fec6c2dc904e123c9ea133c8713b080
Alain Buisseret <me@alain.dev>
2021-09-26 23:37:32 +0200

    test reactions

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,102 @@
 use dotenv::dotenv;
-use std::env;
 use serenity::{
     async_trait,
-    model::{channel::Message, gateway::Ready},
-    prelude::*,
+    client::{Client, Context, EventHandler},
+    model::{
+        channel::Message,
+        gateway::Ready,
+        event::TypingStartEvent,
+        misc::Mentionable
+    },
+    framework::standard::{
+        StandardFramework,
+        CommandResult,
+        macros::{
+            command,
+            group
+        }
+    },
+    // http::AttachmentType,
 };
+
+use std::env;
+// use std::path::Path;
+
+#[group]
+#[commands(ping, hello)]
+struct General;
+
+#[command]
+async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.reply(ctx, "Pong!").await?;
+    Ok(())
+}
+
+#[command]
+async fn hello(ctx: &Context, msg: &Message) -> CommandResult {
+    let _msg = msg.channel_id.send_message(&ctx.http, |m| {
+        m.content("Hello");
+        m.embed(|e| {
+            e.title("This is a title");
+            e.description("This is a description");
+            //e.image("attachment://ferris_eyes.png");
+            e.fields(vec![
+                ("This is the first field", "This is a field body", true),
+                ("This is the second field", "Both of these fields are inline", true),
+            ]);
+            e.field("This is the third field", "This is not an inline field", false);
+            e.footer(|f| {
+                f.text("This is a footer");
+
+                f
+            });
+            e
+        });
+
+        //m.add_file(AttachmentType::Path(Path::new("./ferris_eyes.png")));
+        m
+    }).await?;
+    Ok(())
+}
 
 struct Handler;
 
 #[async_trait]
 impl EventHandler for Handler {
-    // Set a handler for the `message` event - so that whenever a new message
-    // is received - the closure (or function) passed will be called.
-    //
-    // Event handlers are dispatched through a threadpool, and so multiple
-    // events can be dispatched simultaneously.
-    async fn message(&self, ctx: Context, msg: Message) {
-        if msg.content == "!ping" {
-            // Sending a message can fail, due to a network error, an
-            // authentication error, or lack of permissions to post in the
-            // channel, so log to stdout when some error happens, with a
-            // description of it.
-            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
-                println!("Error sending message: {:?}", why);
-            }
-        }
-    }
-
-    // Set a handler to be called on the `ready` event. This is called when a
-    // shard is booted, and a READY payload is sent by Discord. This payload
-    // contains data like the current user's guild Ids, current user data,
-    // private channels, and more.
-    //
-    // In this case, just print what the current user's username is.
     async fn ready(&self, _: Context, ready: Ready) {
         println!("{} is connected!", ready.user.name);
+    }
+
+    async fn typing_start(&self, ctx: Context, ev: TypingStartEvent) {
+        match ev.user_id.to_user(&ctx.http).await {
+            Ok(u) => {
+                let _ = ev.channel_id.send_message(&ctx.http, |m| {
+                    m.content(format_args!(
+                        "{user} est entrain d'écrire... ça sera sûrement très intéressant 🙄",
+                        user = u.mention())
+                    );
+                    m
+                }).await;
+
+                tokio::select! {
+                    Some(r) = u.await_reaction(&ctx.shard) => {
+                        eprintln!("{:?}", r);
+                        let _ = ev.channel_id.send_message(&ctx.http, |m|{
+                            m.content("Hahahaha !");
+                            m
+                        }).await;
+                    },
+                    Some(rep) = u.await_reply(&ctx.shard) => {
+                        eprintln!("{:?}", rep);
+                        let _ = ev.channel_id.send_message(&ctx.http, |m|{
+                            m.content("C'est cela oui !");
+                            m
+                        }).await;
+                    }
+                }
+            }
+            Err(_) => {}
+        }
     }
 }
 
@@ -42,20 +104,20 @@ impl EventHandler for Handler {
 async fn main() {
     let _ = dotenv();
 
-    // Configure the client with your Discord bot token in the environment.
+    let framework = StandardFramework::new()
+        .configure(|c| c.prefix("~")) // set the bot's prefix to "~"
+        .group(&GENERAL_GROUP);
+
+    // Login with a bot token from the environment
     let token = env::var("DISCORD_TOKEN").expect("token");
+    let mut client = Client::builder(token)
+        .event_handler(Handler)
+        .framework(framework)
+        .await
+        .expect("Error creating client");
 
-    // Create a new instance of the Client, logging in as a bot. This will
-    // automatically prepend your bot token with "Bot ", which is a requirement
-    // by Discord for bot users.
-    let mut client =
-        Client::builder(token).event_handler(Handler).await.expect("Err creating client");
-
-    // Finally, start a single shard, and start listening to events.
-    //
-    // Shards will automatically attempt to reconnect, and will perform
-    // exponential backoff until it reconnects.
+    // start listening for events by starting a single shard
     if let Err(why) = client.start().await {
-        println!("Client error: {:?}", why);
+        println!("An error occurred while running the client: {:?}", why);
     }
 }
